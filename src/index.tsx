cc8130ead3ecd0f1e9d7bb8fdbe32f21e2339c5d
fahmij8 <fahmijabbar12@gmail.com>
2022-12-11 01:45:47 +0700

    refactor: remove unnecessary code
    adjust file according to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Routes from './routes';
+import { Global, css } from '@emotion/react';
+import { fonts } from '@workday/canvas-kit-react-fonts';
+import { globalStyles } from './styles';
+import 'allotment/dist/style.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const App = () => {
+  return (
+    <>
+      <Global styles={css(fonts, globalStyles)} />
+      <Routes />
+    </>
+  );
+};
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
