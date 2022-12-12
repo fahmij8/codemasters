d3d006ceb46af2842b88b4f54e2832b3055fdcc9
fahmij8 <fahmijabbar12@gmail.com>
2022-12-12 18:16:05 +0700

    feat: add context type

--- src/types/context/index.ts
+++ src/types/context/index.ts
@@ -1,14 +1,17 @@
-import type { Dispatch } from 'react';
+import type { Dispatch, RefObject, ReactNode } from 'react';
+import type { ToastProps } from '@workday/canvas-kit-react';
 
 export interface StructInitialContext {
   code: {
     html: string;
     css: string;
     js: string;
+    full: string;
   };
+  taskFinished: number;
 }
 
-type ActionTypes = 'SET_HTML' | 'SET_CSS' | 'SET_JS';
+type ActionTypes = 'SET_HTML' | 'SET_CSS' | 'SET_JS' | 'SET_FULL' | 'SET_TASK';
 
 type Distribute<T> = T extends any ? { type: T; payload?: any } : never;
 
@@ -16,4 +19,21 @@ export type ActionContext = Distribute<ActionTypes>;
 
 export interface StructContext extends StructInitialContext {
   dispatch: Dispatch<ActionContext>;
+}
+
+export interface AppToastProps extends Omit<ToastProps, 'children'> {
+  id: string;
+  variant: 'success' | 'error' | 'warning';
+  message: string;
+}
+
+export interface AppModalProps {
+  title: string;
+  children: ReactNode;
+  elementRef?: RefObject<HTMLDivElement>;
+  agreeWord: string;
+  withCancel?: boolean;
+  cancelWord?: string;
+  open?: boolean;
+  onHide?: () => void;
 }
